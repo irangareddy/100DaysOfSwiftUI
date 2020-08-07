5e7203a1322df26f3b30554ef2091c9b0b873fe2
irangareddy <itsmerr17@gmail.com>
2020-08-07 23:56:11 +0530

    Day6/100DaysOfSwiftUI

---
+++ Day6.playground/Contents.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+// MARK:- Creating basic closures
+
+let greet = {
+    print("Welcome to the Day6")
+}
+
+greet()
+
+// MARK:- Accepting parameters in a closure
+
+let greetWithName = { (name: String) in
+    print("Welcome to the Day6, \(name)")
+}
+ 
+greetWithName("Ranga")
+
+// MARK:- Returning values from a closure
+
+let wishWithReturn = { (name: String, age: Int) -> String in
+    return "Wish a very Happy Brithday, \(name) and your turning \(age) today!!!"
+}
+
+print(wishWithReturn("Ranga", 20))
+
+// With Out Passing Parameters
+
+let isMyBirthday = { () -> Bool in
+    if("TodayDate" == "MyBirthdayDate") {
+        return true
+    }
+    return false
+}
+
+isMyBirthday()
+
+
+// MARK:- Closures as parameters
+
+let resignation = { (name: String) in
+    print("Dear \(name), I'm outta here!")
+}
+func printDocument(contents: () -> Void) {
+    print("Connecting to printer...")
+    print("Sending document...")
+    contents()
+}
+printDocument {
+    resignation("Ranga")
+}
+
+
+let writting = {
+    print("I am Writting")
+}
+
+func whatAreYouDoing(name: String, action: () -> Void) {
+    print("\(name) said, " )
+    action()
+}
+
+
+whatAreYouDoing(name: "Ranga", action: writting)
+
+// MARK:- Trailing closure syntax
+
+printDocument {
+    print("Do some Action")
+}
+
+
+whatAreYouDoing(name: "Ranga") {
+    print("lets Design something")
+}
+
+// Print is also a function
--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Paul Hudson free 100DaysOfSwiftUI Course
 | 2 | Swift | Arrays, Sets, Tuples, Dictionaries and Enumertions with associted values and raw values|
 | 3 | Swift | Operations and Conditions|
 | 4 | Swift | Loops |
-| 5 | Swift |Functions|
+| 5 | Swift |Functions, Parameters, and Errors|
+| 6 | Swift |Cloures Part 1|
